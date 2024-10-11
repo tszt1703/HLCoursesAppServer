c76274ce98d77925f4f2d95fc52336789457ff5a
Anastasiia <belka.1024.xxx@gmail.com>
2024-10-11 14:40:35 +0700

    Create project

---
+++ src/main/java/org/example/hlcoursesappserver/HlCoursesAppServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.hlcoursesappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlCoursesAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlCoursesAppServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/hlcoursesappserver/HlCoursesAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hlcoursesappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlCoursesAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
